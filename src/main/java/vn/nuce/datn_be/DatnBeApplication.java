7f56f71fe277be2c6c6b65c5408fbae99baa35de
DuongNB <blackdiamond6129@gmail.com>
2022-02-14 16:56:59 +0700

    allow global cors

--- src/main/java/vn/nuce/datn_be/DatnBeApplication.java
+++ src/main/java/vn/nuce/datn_be/DatnBeApplication.java
@@ -2,6 +2,9 @@ package vn.nuce.datn_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DatnBeApplication {
@@ -10,4 +13,13 @@ public class DatnBeApplication {
         SpringApplication.run(DatnBeApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
