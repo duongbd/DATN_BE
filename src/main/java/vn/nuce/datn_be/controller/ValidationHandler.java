2656b7e1460f975aea7ca54847553eb1e7bcbed3
DuongNB <blackdiamond6129@gmail.com>
2022-02-17 15:42:02 +0700

    validate global dto

--- src/main/java/vn/nuce/datn_be/controller/ValidationHandler.java
+++ src/main/java/vn/nuce/datn_be/controller/ValidationHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import vn.nuce.datn_be.model.dto.ResponseBody;
@@ -13,6 +14,7 @@ import vn.nuce.datn_be.model.enumeration.ResponseCode;
 import java.util.HashMap;
 import java.util.Map;
 
+@ControllerAdvice
 public class ValidationHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
