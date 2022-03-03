ac2d11fec092a74d732ef07e73b8fb8d1c30a993
DuongNB <blackdiamond6129@gmail.com>
2022-03-03 18:02:04 +0700

    schedule and room

--- src/main/java/vn/nuce/datn_be/services/ScheduledTasks.java
+++ src/main/java/vn/nuce/datn_be/services/ScheduledTasks.java
@@ -53,4 +53,33 @@ public class ScheduledTasks {
         });
         log.info("After sleep: " + new Date());
     }
+
+    @Scheduled(fixedRate = 60000)
+    public void endRoomWhenTimeOn() throws InterruptedException {
+        Date start = new Date();
+        log.info("Before sleep: " + start);
+        Calendar startCalendar = Calendar.getInstance();
+        Calendar desCalendar = Calendar.getInstance();
+        Calendar nowCalendar = Calendar.getInstance();
+        startCalendar.setTime(start);
+        desCalendar.setTime(new Date());
+        desCalendar.set(Calendar.SECOND, 0);
+        desCalendar.add(Calendar.MINUTE, 5);
+        nowCalendar.add(Calendar.SECOND, 60 - nowCalendar.get(Calendar.SECOND));
+        List<Room> rooms = roomService.getAllRoomTimeUp(DatnUtils.cvtToGmt(desCalendar.getTime(), 7));
+        TimeUnit.MILLISECONDS.sleep(nowCalendar.getTime().getTime() - new Date().getTime());
+        rooms.forEach(room -> {
+            if (room.getStartTime().before(DatnUtils.cvtToGmt(new Date(), 7)) && room.getEndTime().before(DatnUtils.cvtToGmt(new Date(), 7))) {
+                room.setRoomStatus(RoomStatus.ENDED);
+                roomService.save(room);
+                LogTime logTime = new LogTime();
+                logTime.setRoomFk(room.getId());
+                logTime.setTimeCreate(DatnUtils.cvtToGmt(new Date(), 7));
+                logTime.setContent("End room");
+                logTimeService.save(logTime);
+                log.info("End room with id: " + room.getId());
+            }
+        });
+        log.info("After sleep: " + new Date());
+    }
 }
